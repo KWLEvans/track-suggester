6f7931962c3796ea8bf9a39ced86d5ba88a016f3
Keith Evans <kwlevans@gmail.com>
2017-01-13 13:58:28 -0800

    finish jquery functionality for track suggestion

--- js/scripts.js
+++ js/scripts.js
@@ -9,49 +9,49 @@ var firstQuestion = true;
 var questions = {
   question1: {
     text: "Q1",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answer1: "C# question 1",
+    answer2: "Rails question 1",
+    answer3: "Android question 1",
+    answer4: "Design question 1",
     nextQuestion: "question2"
   },
   question2: {
     text: "Q2",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answer1: "C# question 2",
+    answer2: "Rails question 2",
+    answer3: "Android question 2",
+    answer4: "Design question 2",
     nextQuestion: "question3"
   },
   question3: {
     text: "Q3",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answer1: "C# question 3",
+    answer2: "Rails question 3",
+    answer3: "Android question 3",
+    answer4: "Design question 3",
     nextQuestion: "question4"
   },
   question4: {
     text: "Q4",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answer1: "C# question 4",
+    answer2: "Rails question 4",
+    answer3: "Android question 4",
+    answer4: "Design question 4",
     nextQuestion: "question5"
   },
   question5: {
     text: "Q5",
-    answer1: "",
-    answer2: "",
-    answer3: "",
-    answer4: "",
+    answer1: "C# question 5",
+    answer2: "Rails question 5",
+    answer3: "Android question 5",
+    answer4: "Design question 5",
     nextQuestion: "suggestion"
   },
   suggestion: {
-    cSharp: "",
-    rails: "",
-    android: "",
-    design: ""
+    cSharp: "It sounds like you should check out our C# track!",
+    rails: "It sounds like you should check out our Ruby on Rails track!",
+    android: "It sounds like you should check out our Java/Android track!",
+    design: "It sounds like you should check out our Design track!"
   }
 }
 
@@ -69,8 +69,38 @@ function updateTotals(answer) {
 
 function checkTotals() {
   var totals = [cSharp, rails, android, design];
-  var final = Math.max(totals);
-  return final.toString();
+  var choices = ["cSharp", "rails", "android", "design"];
+  var max = totals[0];
+  var maxIndex = 0;
+  var maxArray = [0];
+
+  for (var i = 1; i < totals.length; i++) {
+    if (totals[i] > max) {
+      max = totals[i];
+      maxIndex = i;
+      maxArray = [maxIndex];
+    } else if (totals[i] === max) {
+      maxIndex = i;
+      maxArray.append(maxIndex);
+    }
+
+    if (maxArray.length === 2) {
+      suggestionsArray = [choices[maxArray[0]], choices[maxArray[1]]];
+    } else {
+      return choices[maxArray[0]];
+    }
+  }
+}
+
+function giveSuggestion() {
+  var totals = checkTotals();
+
+  if (Array.isArray(totals)) {
+    console.log("is array");
+    debugger;
+  } else {
+    return questions.suggestion[totals];
+  }
 }
 
 
@@ -103,12 +133,14 @@ $(function() {
     } else if (nextQuestion === "suggestion") {
       updateTotals(answer);
 
-      var finalSuggestion = checkTotals();
-      var suggestionPath = questions["suggestion"];
+      var finalSuggestion = giveSuggestion();
 
-      $(".panel-body form").remove();
-      $("#suggestion").text(suggestionPath[finalSuggestion]);
+      $("form").remove();
+      $("#question").text("Thanks for taking our quiz.");
+      $("#suggestion").prepend("<h3>" + finalSuggestion + "</h3>");
       $("#suggestion").show();
+
+      event.preventDefault();
     } else {
       updateTotals(answer);
       populateFields(nextQuestion);
