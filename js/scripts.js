c8d357d734688d97d9c6ccdc3a9e8acdebeb2fd7
Keith Evans <kwlevans@gmail.com>
2017-01-13 11:36:59 -0800

    add jquery functionality to display questions and track score

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,121 @@ var cSharp = 0;
 var rails = 0;
 var android = 0;
 var design = 0;
-var questionCount = 0;
+var firstQuestion = true;
+
+var questions = {
+  question1: {
+    text: "Q1",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    nextQuestion: "question2"
+  },
+  question2: {
+    text: "Q2",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    nextQuestion: "question3"
+  },
+  question3: {
+    text: "Q3",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    nextQuestion: "question4"
+  },
+  question4: {
+    text: "Q4",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    nextQuestion: "question5"
+  },
+  question5: {
+    text: "Q5",
+    answer1: "",
+    answer2: "",
+    answer3: "",
+    answer4: "",
+    nextQuestion: "suggestion"
+  },
+  suggestion: {
+    cSharp: "",
+    rails: "",
+    android: "",
+    design: ""
+  }
+}
 
 function updateTotals(answer) {
   if (answer === "1") {
-    cSharp ++;
+    cSharp++;
   } else if (answer === "2") {
-    rails ++;
+    rails++;
   } else if (answer === "3") {
-    android ++;
+    android++;
   } else {
-    design ++;
+    design++;
   }
 }
+
+function checkTotals() {
+  var totals = [cSharp, rails, android, design];
+  var final = Math.max(totals);
+  return final.toString();
+}
+
 
 //Front-end
 
 $(function() {
+
+  function populateFields(nextQuestion) {
+    var question = questions[nextQuestion];
+    $("#question").text(question.text);
+    $("#answerText1").text(question.answer1);
+    $("#answerText2").text(question.answer2);
+    $("#answerText3").text(question.answer3);
+    $("#answerText4").text(question.answer4);
+  }
+
   $("form").submit(function(event) {
-    if (questionCount === 0) {
+    var answer = $("input:radio[name=answer]:checked").val();
+
+    if (firstQuestion && answer === "3") {
+      $("#question").text("Thanks for checking us out!");
+      $(".panel-body form").remove();
+      $(".refresh").show();
+    } else if (firstQuestion) {
+      nextQuestion = "question1";
+      populateFields(nextQuestion);
       $("#answer4").show();
+      nextQuestion = "question2";
+      firstQuestion = false;
+    } else if (nextQuestion === "suggestion") {
+      updateTotals(answer);
+
+      var finalSuggestion = checkTotals();
+      var suggestionPath = questions["suggestion"];
+
+      $(".panel-body form").remove();
+      $("#suggestion").text(suggestionPath[finalSuggestion]);
+      $("#suggestion").show();
+    } else {
+      updateTotals(answer);
+      populateFields(nextQuestion);
+      nextQuestion = questions[nextQuestion].nextQuestion;
     }
 
-    questionCount ++;
-
     event.preventDefault();
+  });
+
+  $(".refresh").click(function() {
+    location.reload();
   });
 });
